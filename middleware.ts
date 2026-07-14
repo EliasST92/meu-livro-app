94b83e02de3e7b9c94795e74d822b1b2b3151148
EliasST92 <157599808+EliasST92@users.noreply.github.com>
2026-07-14 01:21:59 -0300

    Add files via upload

---
+++ middleware.ts
@@ -0,0 +1,2 @@
+export { default } from 'next-auth/middleware';
+export const config={matcher:['/dashboard/:path*','/livros/:path*','/planos/:path*','/comunidade/:path*','/ideias/:path*','/conta/:path*']};
